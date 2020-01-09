42af69db78cfdeffed7e53abb09ecbcf996cc428
Trey <a1306575900@gmail.com>
2020-01-09 23:52:52 +0800

    PropertiesResourceReader and Test

--- festival-value/src/test/java/cn/bdqfork/value/reader/PropertiesResourceTest.java
+++ festival-value/src/test/java/cn/bdqfork/value/reader/PropertiesResourceTest.java
@@ -1,4 +1,22 @@
 package cn.bdqfork.value.reader;
 
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author fbw
+ * @since 2020/1/9
+ */
 public class PropertiesResourceTest {
+
+    @Test
+    public void testLoadProperties() throws Throwable {
+        ResourceReader resourceReader = new PropertiesResourceReader("jdbcConfig.properties");
+        assertEquals(resourceReader.readProperty("driver"), "com.mysql.cj.jdbc.driver");
+        assertEquals(resourceReader.readProperty("username"), "Trey");
+        assertEquals(resourceReader.readProperty("password"), "1234");
+    }
 }
