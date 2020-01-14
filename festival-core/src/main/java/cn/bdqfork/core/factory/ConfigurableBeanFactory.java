fe15f2fc6ff133ad442b18bf6a91b4eb2b169ea0
bdqsky <bdqsky@163.com>
2020-01-14 16:16:35 +0800

    refactor configurable

--- festival-core/src/main/java/cn/bdqfork/core/factory/ConfigurableBeanFactory.java
+++ festival-core/src/main/java/cn/bdqfork/core/factory/ConfigurableBeanFactory.java
@@ -1,12 +1,14 @@
 package cn.bdqfork.core.factory;
 
 import cn.bdqfork.core.factory.processor.BeanPostProcessor;
+import cn.bdqfork.core.factory.registry.BeanDefinitionRegistry;
+import cn.bdqfork.core.factory.registry.SingletonBeanRegistry;
 
 /**
  * @author bdq
  * @since 2019/12/15
  */
-public interface ConfigurableBeanFactory extends BeanFactory {
+public interface ConfigurableBeanFactory extends BeanFactory, SingletonBeanRegistry, BeanDefinitionRegistry {
 
     void setParentBeanFactory(BeanFactory beanFactory);
 
