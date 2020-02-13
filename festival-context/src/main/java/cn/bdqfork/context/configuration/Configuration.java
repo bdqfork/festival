90a3e8931b1c6b80185aaf15241a41b9d67b29ad
bdqsky <bdqsky@163.com>
2020-02-13 19:34:28 +0800

    add location property to Configuration

--- festival-context/src/main/java/cn/bdqfork/context/configuration/Configuration.java
+++ festival-context/src/main/java/cn/bdqfork/context/configuration/Configuration.java
@@ -15,6 +15,11 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 public @interface Configuration {
     /**
+     * 配置文件路径，如果指定了配置文件路径，则将会从指定配置文件中读取属性。
+     */
+    String location() default "";
+
+    /**
      * 配置项的前缀
      */
     String prefix() default "";
