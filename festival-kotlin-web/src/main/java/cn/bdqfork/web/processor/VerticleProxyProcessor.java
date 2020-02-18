24c258e805f32c92adcd9a2700301a6771315337
bdqsky <bdqsky@163.com>
2020-02-18 10:03:32 +0800

    add java version

--- festival-kotlin-web/src/main/java/cn/bdqfork/web/processor/VerticleProxyProcessor.java
+++ festival-kotlin-web/src/main/java/cn/bdqfork/web/processor/VerticleProxyProcessor.java
@@ -8,8 +8,6 @@ import cn.bdqfork.core.util.AnnotationUtils;
 import cn.bdqfork.core.util.AopUtils;
 import cn.bdqfork.web.VertxAware;
 import cn.bdqfork.web.annotation.VerticleMapping;
-import cn.bdqfork.web.service.VerticleProxyHandler;
-import cn.bdqfork.web.service.ServiceVerticle;
 import io.vertx.core.Vertx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
